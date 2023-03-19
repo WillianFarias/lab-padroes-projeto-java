44645621057cd854ed03aa30d33e7b9cbedd57d3
Willian Farias <williansousafarias@gmail.com>
2023-03-19 14:11:06 -0300

    Strategy

--- src/one/digitalinnovation/gof/Test.java
+++ src/one/digitalinnovation/gof/Test.java
@@ -1,14 +1,38 @@
 package one.digitalinnovation.gof;
 
+import one.digitalinnovation.gof.singleton.SingletonLazy;
+import one.digitalinnovation.gof.strategy.Comportamento;
+import one.digitalinnovation.gof.strategy.ComportamentoAgressivo;
+import one.digitalinnovation.gof.strategy.ComportamentoDefensivo;
+import one.digitalinnovation.gof.strategy.ComportamentoNormal;
+import one.digitalinnovation.gof.strategy.Robo;
+
 public class Test {
 
 	public static void main(String[] args) {
 
+		//Singleton
 		SingletonLazy lazy = SingletonLazy.getInstancia();
 		System.out.println(lazy);
 		
 		SingletonLazy novoLazy = SingletonLazy.getInstancia();
 		System.out.println(novoLazy);
+		
+		//Strategy
+		Comportamento normal = new ComportamentoNormal();
+		Comportamento defensivo = new ComportamentoDefensivo();
+		Comportamento agressivo = new ComportamentoAgressivo();
+		
+		Robo robo = new Robo();
+		
+		robo.setComportamento(normal);
+		robo.mover();
+		
+		robo.setComportamento(defensivo);
+		robo.mover();
+		
+		robo.setComportamento(agressivo);
+		robo.mover();
 	}
 
 }
